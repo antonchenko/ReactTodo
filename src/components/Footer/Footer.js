f961123f0e6a99c421098041f82fcbe57081a0ef
Daria Anton <antonchenkodaria20@gmail.com>
2021-06-02 14:53:03 +0300

    delete styles from footer's button

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,16 +6,16 @@ import Button from '@material-ui/core/Button';
 const Footer = ({ count }) => (<footer>
 <div className={styles.footer}>items left: {count}</div>
 <div className={styles.button}>
-    <Button href="#text-buttons" color="#d3d3d3" style={{ fontSize: '8px'}}>
+    <Button href="#text-buttons" style={{ fontSize: '8px'}}>
     All
     </Button>
-    <Button href="#text-buttons" color="#d3d3d3" style={{ fontSize: '8px'}}>
+    <Button href="#text-buttons" style={{ fontSize: '8px'}}>
     Active
     </Button>
-    <Button href="#text-buttons" color="#d3d3d3" style={{ fontSize: '8px'}}>
+    <Button href="#text-buttons" style={{ fontSize: '8px'}}>
     Completed
     </Button>
-    <Button href="#text-buttons" color="#d3d3d3" style={{ fontSize: '8px'}}>
+    <Button href="#text-buttons" style={{ fontSize: '8px'}}>
     Clear completed
     </Button>
 </div>
